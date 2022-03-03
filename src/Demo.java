fabbf79f00f231d4b1ffda681fedb744ec0dbabd
AbhishekWankhede0409 <100713298+AbhishekWankhede0409@users.noreply.github.com>
2022-03-03 09:30:28 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,7 @@
 
 public class Demo {
   public static void main(String[] args{
-    System.out.println("Hii From Local"):
+    System.out.println("Hii From Local");
     //Hii from github
+    //hii from github(3 mar)
 }
